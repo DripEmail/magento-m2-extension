0c8c5d6c41d188d10afc60746615a113bbe7b42f
Viacheslav Kolosov <skolosov@robofirm.com>
2018-04-10 17:04:22 +0300

    config for sync buttons

---
+++ Block/System/Config/Sync/Button.php
@@ -0,0 +1,61 @@
+<?php
+namespace Drip\Connect\Block\System\Config\Sync;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+abstract class Button extends \Magento\Config\Block\System\Config\Form\Field
+{
+    const BUTTON_TEMPLATE = '';
+
+    /**
+     * Set template to itself
+     *
+     * @return $this
+     */
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        if (!$this->getTemplate()) {
+            $this->setTemplate(static::BUTTON_TEMPLATE);
+        }
+        return $this;
+    }
+
+    /**
+     * Render button
+     *
+     * @param  \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        // Remove scope label
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+    /**
+     * Return ajax url for button
+     *
+     * @return string
+     */
+    abstract function getAjaxUrl();
+
+    /**
+     * Get the button and scripts contents
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $originalData = $element->getOriginalData();
+        $this->addData(
+            [
+                'html_id' => $element->getHtmlId(),
+                'button_label' => __($originalData['button_label']),
+            ]
+        );
+        return $this->_toHtml();
+    }
+}
