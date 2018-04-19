88dbae8d5100e04c6d678cc7a195eb0a1c6cc6b1
Nick Bartlett <nickjbartlett@gmail.com>
2018-04-19 20:07:19 +0000

    changed out translations project specific commands to drip

--- .bitbucket-pipelines/update_parent_projects.sh
+++ .bitbucket-pipelines/update_parent_projects.sh
@@ -8,13 +8,8 @@
 # Update drip/connect module and push back to parent projects
 #ssh -q webuser@uat-dripm2.robofirm.net "
 #  cd /tmp &&
-#  ([[ -d translations-magento-2-1 ]] || git clone git@bitbucket.org:robofirm/translations-magento-2-1.git --depth 1) &&
-#  cd translations-magento-2-1 &&
-#  composer update drip/module-connect &&
-#  (git diff composer.lock && git add composer.lock && git commit -a -m \"Updated drip/module-connect to latest master\" && git push || true) && 
-#  cd .. &&
-#  ([[ -d translations-magento-2-0 ]] || git clone git@bitbucket.org:robofirm/translations-magento-2-0.git --depth 1) &&
-#  cd translations-magento-2-0 &&
-#  composer update drip/module-connect &&
-#  (git diff composer.lock && git add composer.lock && git commit -a -m \"Updated drip/module-connect to latest master\" && git push || true)
+#  ([[ -d dripm2 ]] || git clone git@bitbucket.org:robofirm/drip-magento2.git --depth 1) &&
+#  cd dripm2 &&
+#  composer update drip/connect &&
+#  (git diff composer.lock && git add composer.lock && git commit -a -m \"Updated drip/connect to latest master\" && git push || true) 
 #") || exit 1
