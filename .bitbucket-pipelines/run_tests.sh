d3834d90d402b627b042f0154c6c66437527b466
Viacheslav Kolosov <skolosov@robofirm.com>
2018-04-23 17:22:07 +0300

    fix file encoding

--- .bitbucket-pipelines/run_tests.sh
+++ .bitbucket-pipelines/run_tests.sh
@@ -3,8 +3,10 @@
 apt-get install -y libxml2-dev libxslt-dev &&
 docker-php-ext-install soap &&
 docker-php-ext-install xsl &&
+# Composer install
 echo "{\"http-basic\":{\"repo.magento.com\":{\"password\":\"$MAGENTO_COMPOSER_PASSWORD\",\"username\":\"$MAGENTO_COMPOSER_USERNAME\"}}}" > auth.json &&
 composer install --quiet &&
+# Run tests
 vendor/bin/phpcs --config-set installed_paths vendor/magento/marketplace-eqp &&
 vendor/bin/phpcs . --ignore=vendor/*  --standard=MEQP2 --severity=10 &&
 vendor/bin/phpunit --configuration phpunit.xml) || exit 1
