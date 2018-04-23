f6025d37c1ff73914c4b34959c57ec9f50c2b1ea
Viacheslav Kolosov <skolosov@robofirm.com>
2018-04-23 17:32:59 +0300

    add php

--- .bitbucket-pipelines/run_tests.sh
+++ .bitbucket-pipelines/run_tests.sh
@@ -7,6 +7,6 @@ docker-php-ext-install xsl &&
 echo "{\"http-basic\":{\"repo.magento.com\":{\"password\":\"$MAGENTO_COMPOSER_PASSWORD\",\"username\":\"$MAGENTO_COMPOSER_USERNAME\"}}}" > auth.json &&
 composer install --quiet &&
 # Run tests
-vendor/bin/phpcs --config-set installed_paths vendor/magento/marketplace-eqp &&
-vendor/bin/phpcs . --ignore=vendor/*  --standard=MEQP2 --severity=10 &&
-vendor/bin/phpunit --configuration phpunit.xml) || exit 1
+php vendor/bin/phpcs --config-set installed_paths vendor/magento/marketplace-eqp &&
+php vendor/bin/phpcs . --ignore=vendor/*  --standard=MEQP2 --severity=10 &&
+php vendor/bin/phpunit --configuration phpunit.xml) || exit 1
