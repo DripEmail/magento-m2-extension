4d3300d2463ad09124607157a4b8b79d46096d12
Viacheslav Kolosov <skolosov@robofirm.com>
2018-04-23 14:15:29 +0000

    run_tests.sh edited online with Bitbucket

--- .bitbucket-pipelines/run_tests.sh
+++ .bitbucket-pipelines/run_tests.sh
@@ -3,12 +3,8 @@
 apt-get install -y libxml2-dev libxslt-dev &&
 docker-php-ext-install soap &&
 docker-php-ext-install xsl &&
-            
-# Composer install
 echo "{\"http-basic\":{\"repo.magento.com\":{\"password\":\"$MAGENTO_COMPOSER_PASSWORD\",\"username\":\"$MAGENTO_COMPOSER_USERNAME\"}}}" > auth.json &&
-composer install --quiet && 
-            
-# Run tests
+composer install --quiet &&
 vendor/bin/phpcs --config-set installed_paths vendor/magento/marketplace-eqp &&
 vendor/bin/phpcs . --ignore=vendor/*  --standard=MEQP2 --severity=10 &&
-vendor/bin/phpunit --configuration phpunit.xml) || exit 1 
+vendor/bin/phpunit --configuration phpunit.xml) || exit 1
