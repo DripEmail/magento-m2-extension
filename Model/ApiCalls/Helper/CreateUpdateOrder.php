8b8f88681cb5521d0164ce495b39b0fd58b97503
Viacheslav Kolosov <skolosov@robofirm.com>
2019-04-09 16:53:03 +0300

    DRIP-173 add version to order activity call

--- Model/ApiCalls/Helper/CreateUpdateOrder.php
+++ Model/ApiCalls/Helper/CreateUpdateOrder.php
@@ -23,15 +23,25 @@ class CreateUpdateOrder
     /** @var \Magento\Framework\App\Config\ScopeConfigInterface */
     protected $scopeConfig;
 
+    /** @var \Magento\Framework\App\ProductMetadataInterface */
+    protected $productMetadata;
+
+    /** @var \Magento\Framework\Module\ResourceInterface */
+    protected $moduleResource;
+
     public function __construct(
         \Drip\Connect\Model\ApiCalls\BaseFactory $connectApiCallsBaseFactory,
         \Drip\Connect\Model\ApiCalls\Request\BaseFactory $connectApiCallsRequestBaseFactory,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\Module\ResourceInterface $moduleResource,
+        \Magento\Framework\App\ProductMetadataInterface $productMetadata,
         $data = []
     ) {
         $this->connectApiCallsBaseFactory = $connectApiCallsBaseFactory;
         $this->connectApiCallsRequestBaseFactory = $connectApiCallsRequestBaseFactory;
         $this->scopeConfig = $scopeConfig;
+        $this->moduleResource = $moduleResource;
+        $this->productMetadata = $productMetadata;
 
         $this->apiClient = $this->connectApiCallsBaseFactory->create([
             'options' => [
@@ -39,6 +49,11 @@ class CreateUpdateOrder
                 'v3' => true,
             ]
         ]);
+
+        if (!empty($data) && is_array($data)) {
+            $data['version'] = 'Magento ' . $this->productMetadata->getVersion() . ', '
+                             . 'Drip Extension ' . $this->moduleResource->getDbVersion('Drip_Connect');
+        }
 
         $this->request = $this->connectApiCallsRequestBaseFactory->create()
             ->setMethod(\Zend_Http_Client::POST)
