f085e92063e542dc76d76e51db81cdc723af77ef
William Johnston <william.johnston@drip.com>
2019-08-21 16:35:33 -0500

    Fix log settings gathering

--- Logger/Handler.php
+++ Logger/Handler.php
@@ -39,7 +39,7 @@ class Handler extends \Magento\Framework\Logger\Handler\Base
     ) {
         // Set whether this is enabled by the customer from their settings.
         $logSettings = $dataObjectFactory->create();
-        $logSettings = $scopeConfig->getValue($this->_logSettingsXpath, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $logSettings->setData($scopeConfig->getValue($this->_logSettingsXpath, \Magento\Store\Model\ScopeInterface::SCOPE_STORE));
 
         $this->isEnabled = $logSettings->getIsEnabled();
 
