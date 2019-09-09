cf9b23411e833e94f1993b553c07e548b0ee5f93
William Johnston <william.johnston@drip.com>
2019-09-09 12:33:59 -0500

    Reset cron before running

--- devtools_m2/cron.sh
+++ devtools_m2/cron.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Reset cron
+docker-compose exec db mysql -u magento -pmagento magento -e "DELETE FROM cron_schedule WHERE job_code LIKE 'drip_%' AND status = 'running';"
+
 # Force cron runs to now.
 docker-compose exec db mysql -u magento -pmagento magento -e "UPDATE cron_schedule SET scheduled_at = NOW() WHERE schedule_id = (SELECT schedule_id FROM (SELECT * FROM cron_schedule) AS inner_cron_schedule WHERE job_code = 'drip_connect_sync_customers' AND status = 'pending' ORDER BY scheduled_at ASC LIMIT 1);"
 docker-compose exec db mysql -u magento -pmagento magento -e "UPDATE cron_schedule SET scheduled_at = NOW() WHERE schedule_id = (SELECT schedule_id FROM (SELECT * FROM cron_schedule) AS inner_cron_schedule WHERE job_code = 'drip_connect_sync_orders' AND status = 'pending' ORDER BY scheduled_at ASC LIMIT 1);"
