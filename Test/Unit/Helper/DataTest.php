127688ee21a19c4d68e37f347a8cb873766e3f25
William Johnston <william.johnston@drip.com>
2019-09-18 09:21:32 -0500

    Useless test

---
+++ Test/Unit/Helper/DataTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drip\Connect\Test\Unit\Helper;
+
+class DataTest extends \PHPUnit\Framework\TestCase
+{
+    public function testFormatDate()
+    {
+        // $dataHelper = new \Drip\Connect\Helper\Data();
+        $this->assertTrue(true);
+    }
+}
