7c170c9783ce30290e6575f1f868a99ef5bad2eb
William Johnston <william.johnston@drip.com>
2019-10-23 15:20:34 -0500

    Pull in cypress-pipe

--- devtools_m2/cypress/support/index.js
+++ devtools_m2/cypress/support/index.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 import chaiString from 'chai-string';
+import 'cypress-pipe'
 
 // Import commands.js using ES2015 syntax:
 import './commands'
