f1aa84cf32da736184f2e7c6ed642d5f966d520f
William Johnston <william.johnston@drip.com>
2019-10-23 14:33:51 -0500

    Run less cron stuff in dev

--- devtools_m2/cron.sh
+++ devtools_m2/cron.sh
@@ -5,4 +5,4 @@ set -e
 ./docker_compose.sh exec -T db mysql -u magento -pmagento magento -e "DELETE FROM cron_schedule WHERE job_code IN ('drip_connect_sync_customers', 'drip_connect_sync_customers');"
 ./docker_compose.sh exec -T db mysql -u magento -pmagento magento -e "INSERT INTO cron_schedule (job_code, status, created_at, scheduled_at) VALUES ('drip_connect_sync_customers', 'pending', NOW(), NOW()), ('drip_connect_sync_orders', 'pending', NOW(), NOW());"
 
-./docker_compose.sh exec -T -u www-data web /bin/bash -c "cd /var/www/html/magento/ && bin/magento cron:run"
+./docker_compose.sh exec -T -u www-data web /bin/bash -c "cd /var/www/html/magento/ && bin/magento cron:run --group default"
