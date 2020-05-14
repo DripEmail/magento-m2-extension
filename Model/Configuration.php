bc2afd634f4e6065abdaa88f3484fde6d9627c92
William Johnston <william.johnston@drip.com>
2020-05-14 16:18:05 -0500

    Rip out of configuration class all the irrelivant code

--- Model/Configuration.php
+++ Model/Configuration.php
@@ -4,18 +4,11 @@ namespace Drip\Connect\Model;
 
 class Configuration
 {
-    const ACCOUNT_ID_PATH = 'dripconnect_general/api_settings/account_id';
-    const API_URL_PATH = 'dripconnect_general/api_settings/url';
-    const API_TIMEOUT_PATH = 'dripconnect_general/api_settings/timeout';
-    const API_KEY_PATH = 'dripconnect_general/api_settings/api_key';
-    const API_INTEGRATION_PARAM = 'dripconnect_general/api_settings/api_integration_param';
-    const CUSTOMER_DATA_STATE_PATH = 'dripconnect_general/actions/sync_customers_data_state';
-    const ORDER_DATA_STATE_PATH = 'dripconnect_general/actions/sync_orders_data_state';
+    const WIS_URL_PATH = 'dripconnect_general/api_settings/wis_url';
+    const INTEGRATION_TOKEN = 'dripconnect_general/api_settings/integration_token';
     const MODULE_ENABLED_PATH = 'dripconnect_general/module_settings/is_enabled';
     const SALT_PATH = 'dripconnect_general/module_settings/salt';
     const LOG_SETTINGS_PATH = 'dripconnect_general/log_settings';
-    const MEMORY_LIMIT_PATH = 'dripconnect_general/api_settings/memory_limit';
-    const BATCH_DELAY_PATH = 'dripconnect_general/api_settings/batch_delay';
 
     /**
      * @var \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
@@ -84,63 +77,14 @@ class Configuration
         return $this->storeManager->getStore($this->storeId)->getWebsiteId();
     }
 
-    public function getAccountId()
+    public function getWisUrl()
     {
-        return $this->getStoreConfig(self::ACCOUNT_ID_PATH);
+        return $this->getStoreConfig(self::WIS_URL_PATH);
     }
 
-    public function getUrl()
+    public function getIntegrationToken()
     {
-        return $this->getStoreConfig(self::API_URL_PATH);
-    }
-
-    public function getTimeout()
-    {
-        return $this->getStoreConfig(self::API_TIMEOUT_PATH);
-    }
-
-    /**
-     * @param int $timeout The timeout in seconds.
-     */
-    public function setTimeout($timeout)
-    {
-        $this->setStoreConfig(self::API_TIMEOUT_PATH, $timeout);
-    }
-
-    public function getApiKey()
-    {
-        return $this->getStoreConfig(self::API_KEY_PATH);
-    }
-
-    public function getApiIntegrationParam()
-    {
-        return $this->getStoreConfig(self::API_INTEGRATION_PARAM);
-    }
-
-    public function getCustomersSyncState()
-    {
-        return $this->getStoreConfig(self::CUSTOMER_DATA_STATE_PATH);
-    }
-
-    /**
-     * @param int $state
-     */
-    public function setCustomersSyncState($state)
-    {
-        $this->setStoreConfig(self::CUSTOMER_DATA_STATE_PATH, $state);
-    }
-
-    public function getOrdersSyncState()
-    {
-        return $this->getStoreConfig(self::ORDER_DATA_STATE_PATH);
-    }
-
-    /**
-     * @param int $state
-     */
-    public function setOrdersSyncState($state)
-    {
-        $this->setStoreConfig(self::ORDER_DATA_STATE_PATH, $state);
+        return $this->getStoreConfig(self::INTEGRATION_TOKEN);
     }
 
     /**
@@ -161,19 +105,6 @@ class Configuration
         return $this->getStoreConfig(self::LOG_SETTINGS_PATH);
     }
 
-    public function getMemoryLimit()
-    {
-        return $this->getStoreConfig(self::MEMORY_LIMIT_PATH);
-    }
-
-    /**
-     * @return int
-     */
-    public function getBatchDelay()
-    {
-        return (int) $this->getStoreConfig(self::BATCH_DELAY_PATH);
-    }
-
     /**
      * @param string $path
      */
@@ -184,20 +115,5 @@ class Configuration
             $this->scope,
             $this->storeId
         );
-    }
-
-    /**
-     * @param string $path
-     * @param mixed $val
-     */
-    protected function setStoreConfig($path, $val)
-    {
-        $this->resourceConfig->saveConfig(
-            $path,
-            $val,
-            $this->scope,
-            $this->storeId
-        );
-        $this->storeManager->getStore($this->storeId)->resetConfig();
     }
 }
