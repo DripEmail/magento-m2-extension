3175e5089c137171f640e49f2372c5d931871f56
Ian Nance <iantnance@gmail.com>
2020-05-20 15:57:30 -0700

    authorization for custom endpoint

--- devtools_m2/cypress/integration/RestApi.feature
+++ devtools_m2/cypress/integration/RestApi.feature
@@ -2,7 +2,12 @@ Feature: REST API Interactions
 
   I want to make plugin settings available via the REST API
 
-  Scenario: Request for account_id
+  Scenario: Authorized request for account_id
     Given I am logged into the admin interface
       And I have configured Drip to be enabled for 'main'
-    Then a REST API request gives the correct response
+    Then an authorized REST API request gives the correct response
+
+  Scenario: Unauthorized equest for account_id
+    Given I am logged into the admin interface
+      And I have configured Drip to be enabled for 'main'
+    Then an unauthorized REST API request gives the correct response
--- devtools_m2/cypress/integration/RestApi/steps.js
+++ devtools_m2/cypress/integration/RestApi/steps.js
@@ -3,9 +3,26 @@ import { mockServerClient } from "mockserver-client"
 
 const Mockclient = mockServerClient("localhost", 1080);
 
-Then('a REST API request gives the correct response', function(site) {
-  cy.request("http://main.magento.localhost:3006/rest/V1/drip/account_id?websiteId=1").then((response) => {
-    expect(response.status).to.eq(200)
-    expect(JSON.parse(response.body)["account_id"]).to.eq('123456')
+Then('an authorized REST API request gives the correct response', function(site) {
+  cy.request({
+    url: "http://main.magento.localhost:3006/rest/V1/integration/admin/token",
+    method: "POST",
+    body: {"username":"admin", "password":"abc1234567890"}
+  }).then((token_response) => {
+    cy.request({
+      url: "http://main.magento.localhost:3006/rest/V1/drip/account_id?websiteId=1",
+      auth: {
+        bearer: token_response.body
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(JSON.parse(response.body)["account_id"]).to.eq('123456')
+    })
+  })
+})
+
+Then('an unauthorized REST API request gives the correct response', function(site) {
+  cy.request({ url: "http://main.magento.localhost:3006/rest/V1/drip/account_id?websiteId=1", failOnStatusCode: false }).then((response) => {
+    expect(response.status).to.eq(401)
   })
 })
