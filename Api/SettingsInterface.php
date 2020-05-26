9c0afd20d1d3edfbe786c00b901d429c347ac041
Ian Nance <iantnance@gmail.com>
2020-05-26 12:45:12 -0700

    more indentation fixes

--- Api/SettingsInterface.php
+++ Api/SettingsInterface.php
@@ -1,19 +1,19 @@
 <?php
 namespace Drip\Connect\Api;
 interface SettingsInterface {
-		/**
-		 * POST for integration settings API
-		 * @param string $websiteId
-		 * @param string $accountParam
-		 * @param string $integrationToken
-		 * @return string
-		 */
-    public function updateSettings($websiteId, $accountParam, $integrationToken);
+	/**
+	 * POST for integration settings API
+	 * @param string $websiteId
+	 * @param string $accountParam
+	 * @param string $integrationToken
+	 * @return string
+	 */
+	 public function updateSettings($websiteId, $accountParam, $integrationToken);
 
-		/**
-		 * POST for integration settings API
-		 * @param string $websiteId]
-		 * @return string
-		 */
-		public function showStatus($websiteId);
+	/**
+	 * POST for integration settings API
+	 * @param string $websiteId]
+	 * @return string
+	 */
+	public function showStatus($websiteId);
 }
