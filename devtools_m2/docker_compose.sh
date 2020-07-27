ba9e4616798dc014d5c4a94a30991d121ee3f074
William Johnston <william.johnston@drip.com>
2020-07-27 13:45:42 -0500

    Switch to test, because some moron (yours truely) defaulted to a state that never ended up being used

--- devtools_m2/docker_compose.sh
+++ devtools_m2/docker_compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-DRIP_COMPOSE_ENV2=${DRIP_COMPOSE_ENV:-"dev"}
+DRIP_COMPOSE_ENV2=${DRIP_COMPOSE_ENV:-"test"}
 
 docker-compose -p "devtools_m2_${DRIP_COMPOSE_ENV2}" -f docker-compose.base.yml -f "docker-compose.${DRIP_COMPOSE_ENV2}.yml" "$@"
