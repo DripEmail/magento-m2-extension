066060eede6f35948a8131e50378da1b8f0dae62
William Johnston <william.johnston@drip.com>
2020-07-28 09:35:28 -0500

    Set admin config programmatically

--- devtools_m2/cypress/support/product_management.js
+++ devtools_m2/cypress/support/product_management.js
@@ -34,6 +34,25 @@ Cypress.Commands.add("createScopes", (desc) => {
   })
 })
 
+Cypress.Commands.add("setConfig", (desc) => {
+  cy.log('Setting Magento config')
+
+  let cmd = 'bin/magento config:set'
+  if (desc['scope']) {
+    cmd += ` --scope="${desc['scope']}"`
+  }
+  if (desc['scopeCode']) {
+    cmd += ` --scope-code="${desc['scopeCode']}"`
+  }
+  cmd += ` "${desc['path']}" "${desc['value']}"`
+
+  cy.exec(`./docker_compose.sh exec -u www-data -T web ${cmd}`, {
+    env: {
+      DRIP_COMPOSE_ENV: 'test'
+    }
+  })
+})
+
 Cypress.Commands.add("runCron", (desc) => {
   cy.log('Running Magento Cron')
   const str = JSON.stringify(desc)
