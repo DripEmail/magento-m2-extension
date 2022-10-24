e5e7f864937ffd933d84f520258a9c7cdf131cec
Diego Orozco <miguel.orozco@drip.com>
2022-10-24 13:01:00 -0500

    Implemented getTimeout method

--- Model/ApiCalls/Base.php
+++ Model/ApiCalls/Base.php
@@ -70,7 +70,7 @@ class Base extends RestapiAbstract
             'uri' => $url,
             'config' => [
                 'useragent' => self::USERAGENT,
-                'timeout' => $config->getTimeout(),
+                'timeout' => $config->getTimeout() / 1000,
             ],
             'logger' => $this->logger,
         ]);
